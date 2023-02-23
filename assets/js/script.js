2fdbb178477bd43a39ece7f6b4537f7b9809d3da
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-02-23 18:06:00 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,19 +19,11 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
-const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
 
 // modal variable
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
-
-// modal toggle function
-const testimonialsModalFunc = function () {
-  modalContainer.classList.toggle("active");
-  overlay.classList.toggle("active");
-}
 
 // add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
@@ -48,13 +40,6 @@ for (let i = 0; i < testimonialsItem.length; i++) {
   });
 
 }
-
-console.log(modalCloseBtn);
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
-
-
 
 // custom select variables
 const select = document.querySelector("[data-select]");
